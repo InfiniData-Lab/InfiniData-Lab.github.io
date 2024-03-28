3f864094adc98d6d6247d426ab962178b4780685
zLizy <z.li-14@tudelft.nl>
2024-03-28 11:00:09 +0100

    edit header and layout

---
+++ src/content/post/amalur.md
@@ -0,0 +1,38 @@
+---
+publishDate: 2023-08-06T00:00:00Z
+title: Amalur
+excerpt: The Convergence of Data Integration an Machin Learning
+image: ~/assets/images/project/amalur-scope.png
+category: project
+tags:
+  - machine-learning
+  - data-integration
+  - federated-learning
+  - model-lake
+metadata:
+  canonical: https://astrowind.vercel.app/amalur
+---
+
+## Abstract
+
+Machine learning (ML) training data is often scat-
+tered across disparate collections of datasets, called data silos.
+This fragmentation poses a major challenge for data-intensive
+ML applications: integrating and transforming data residing
+in different sources demand a lot of manual work and com-
+putational resources. With data privacy constraints, data often
+cannot leave the premises of data silos; hence model training
+should proceed in a decentralized manner. In this work, we
+present a vision of bridging traditional data integration (DI)
+techniques with the requirements of modern machine learn-
+ing systems. We explore the possibilities of utilizing metadata
+obtained from data integration processes for improving the
+effectiveness, efficiency, and privacy of ML models. Towards
+this direction, we analyze ML training and inference over data
+silos. Bringing data integration and machine learning together, we
+highlight new research opportunities from the aspects of systems,
+representations, factorized learning, and federated learning
+
+## Example
+
+![Target](~/assets/images/project/amalur/fig-2-exp.png)
--- src/navigation.js
+++ src/navigation.js
@@ -1,73 +1,37 @@
 import { getHomePermalink, getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
-  links: [
-    {
-      text: 'Home',
-      href: getHomePermalink(),
-    },
-    {
-      text: 'People',
-      href: 'people',
-    },
-    {
-      text: 'Landing',
-      links: [
+  links:[
+        { text: 'Home', href: getPermalink() },
+        { text: 'About', href: getPermalink('#about') },
+        { text: 'People', href: getPermalink('people') },
+        { text: 'Publications', href: getPermalink('#publications') },
+        { text: 'Projects', href: getPermalink('#projects') },
         {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
+          text: 'Blog',
+          links: [
+            {
+              text: 'Blog List',
+              href: getBlogPermalink(),
+            },
+            {
+              text: 'About our team',
+              href: getPermalink('amalur', 'post'),
+            },
+            {
+              text: 'Projects',
+              href: getPermalink('project', 'category'),
+            },
+            {
+              text: 'Model Lake',
+              href: getPermalink('model-lake', 'tag'),
+            },
+            {
+              text: 'Privacy',
+              href: getPermalink('privacy', 'tag'),
+            },
+          ],
         },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
   ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
